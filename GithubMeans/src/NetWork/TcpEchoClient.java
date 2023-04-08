b842ccc2d0d887a6df9939717aa80fa4e15f9b33
M-DGZ <130123265+M-DGZ@users.noreply.github.com>
2023-04-08 10:40:40 +0800

    client

---
+++ GithubMeans/src/NetWork/TcpEchoClient.java
@@ -0,0 +1,43 @@
+package NetWork;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class TcpEchoClient {
+    private Socket socket = null;
+    private static final String ServerIP = "127.0.0.1";
+    private static final int ServerPort = 9090;
+    public TcpEchoClient() throws IOException {
+        socket = new Socket(ServerIP,ServerPort);
+    }
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        try(InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream()) {
+            Scanner scannerFromSocket = new Scanner(inputStream);
+            PrintWriter printWriter = new PrintWriter(outputStream);
+            while (true) {
+                System.out.print("->:");
+                String request = scanner.next();
+                printWriter.println(request);
+                String response = scannerFromSocket.next();
+                System.out.printf("req %s: resp resp",request,response);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (true) {
+
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        TcpEchoClient tcpEchoClient = new TcpEchoClient();
+        tcpEchoClient.start();
+
+    }
+}
