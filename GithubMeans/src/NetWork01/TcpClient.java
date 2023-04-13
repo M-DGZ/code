ed584260c90582fd5dad302874dae65d5b1bd064
dong-guozhen <2029187514@qq.com>
2023-04-13 21:38:05 +0800

    tcp

--- GithubMeans/src/NetWork01/TcpClient.java
+++ GithubMeans/src/NetWork01/TcpClient.java
@@ -13,7 +13,7 @@ public class TcpClient {
     private int ServerPort = 9090;
 
     public TcpClient() throws IOException {
-        socket = new Socket(ServerIP,ServerPort);  //这个操作会和tcp服务器建立了连接  三次握手发生在这个阶段
+        socket = new Socket(ServerIP, ServerPort);  //这个操作会和tcp服务器建立了连接  三次握手发生在这个阶段
     }
 
     /**
@@ -22,23 +22,22 @@ public class TcpClient {
     private void start() {
         Scanner scanner = new Scanner(System.in);
 
-            try(InputStream inputStream = socket.getInputStream();
-                OutputStream outputStream = socket.getOutputStream()) {
-                PrintWriter printWriter = new PrintWriter(outputStream);
-                Scanner scannerFromSocket = new Scanner(inputStream);
-                while (true) {
-                    System.out.print("->:");
-                    //用户从输入请求，然后构造成数据报，然后发送，最后接收服务器的响应
-                    String request = scanner.next();
-                    printWriter.println(request);  //写网卡
-                    printWriter.flush();
-                    String response = scannerFromSocket.next();
-                    System.out.printf("req:%s resp:%s\n", request, response);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+        try (InputStream inputStream = socket.getInputStream();
+             OutputStream outputStream = socket.getOutputStream()) {
+            PrintWriter printWriter = new PrintWriter(outputStream);
+            Scanner scannerFromSocket = new Scanner(inputStream);
+            while (true) {
+                System.out.print("->:");
+                //用户从输入请求，然后构造成数据报，然后发送，最后接收服务器的响应
+                String request = scanner.next();
+                printWriter.println(request);  //写网卡
+                printWriter.flush();
+                String response = scannerFromSocket.next();
+                System.out.printf("req:%s resp:%s\n", request, response);
             }
-
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) throws IOException {
